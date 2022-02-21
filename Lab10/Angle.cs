564d36aacc9ca72e2913287324942f4f6afe389a
Михаил <stulstoldevyat@gmail.com>
2022-02-21 19:46:54 +0300

    Добавьте файлы проекта.

---
+++ Lab10/Angle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10
+{
+    class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (value >= 360)
+                {
+                    gradus = value % 360;
+                }
+                else
+                {
+                    gradus = value;
+                }
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                Gradus += value / 60;
+                min = value % 60;
+            }
+        }
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                Min += value / 60;
+                sec = value % 60;
+            }
+        }
+
+        public Angle(int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+        public double ToRadian()
+        {
+            return (gradus + (double)min / 60 + (double)sec / 60 / 60) * Math.PI / 180;
+        }
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите данные");
+            int gradus_ = Convert.ToInt32(Console.ReadLine());
+            int minute_ = Convert.ToInt32(Console.ReadLine());
+            int sec_ = Convert.ToInt32(Console.ReadLine());
+
+            Angle angle = new Angle(gradus_, minute_, sec_);
+
+            double toRadian = angle.ToRadian();
+            Console.WriteLine("Угол {0}d {1}\' {2}\"", angle.Gradus, angle.Min, angle.Sec);
+            Console.WriteLine("Угол в радианах равен = {0}", toRadian);
+
+            Console.ReadKey();
+        }
+    }
+}
